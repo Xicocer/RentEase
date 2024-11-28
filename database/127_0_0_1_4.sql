e529e6f031ca5da2e696a4a1ffae21f91d8a10cf
jokeyprog <igorblag243@gmail.com>
2024-11-28 23:24:03 +0300

    Fix

--- database/127_0_0_1_4.sql
+++ database/127_0_0_1_4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Ноя 28 2024 г., 11:53
+-- Время создания: Ноя 28 2024 г., 21:20
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -26,13 +26,27 @@ USE `rentease1`;
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `category`
+--
+
+CREATE TABLE `category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `category` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `feedback`
 --
 
 CREATE TABLE `feedback` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `id_user` bigint(20) UNSIGNED NOT NULL,
-  `feed_text` text NOT NULL
+  `feed_text` text NOT NULL,
+  `id_item` bigint(20) UNSIGNED NOT NULL,
+  `mark` int(5) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -43,10 +57,25 @@ CREATE TABLE `feedback` (
 
 CREATE TABLE `items` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `title` varchar(20) NOT NULL,
+  `title` text NOT NULL,
   `price` int(11) NOT NULL,
   `img` text NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `id_category` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rent`
+--
+
+CREATE TABLE `rent` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `id_item` bigint(20) UNSIGNED NOT NULL,
+  `rent` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -57,12 +86,10 @@ CREATE TABLE `items` (
 
 CREATE TABLE `user` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
-  `age` int(2) NOT NULL,
-  `login` text NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `email` text NOT NULL,
   `password` text NOT NULL,
-  `is_admin` tinyint(1) NOT NULL
+  `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -70,17 +97,34 @@ CREATE TABLE `user` (
 --
 
 --
+-- Индексы таблицы `category`
+--
+ALTER TABLE `category`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- Индексы таблицы `feedback`
 --
 ALTER TABLE `feedback`
   ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_item` (`id_item`),
   ADD KEY `id_user` (`id_user`);
 
 --
 -- Индексы таблицы `items`
 --
 ALTER TABLE `items`
-  ADD UNIQUE KEY `id` (`id`);
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_category` (`id_category`);
+
+--
+-- Индексы таблицы `rent`
+--
+ALTER TABLE `rent`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_items` (`id_item`);
 
 --
 -- Индексы таблицы `user`
@@ -93,6 +137,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `feedback`
 --
 ALTER TABLE `feedback`
@@ -102,6 +152,12 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT для таблицы `items`
 --
 ALTER TABLE `items`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `rent`
+--
+ALTER TABLE `rent`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -118,7 +174,22 @@ ALTER TABLE `user`
 -- Ограничения внешнего ключа таблицы `feedback`
 --
 ALTER TABLE `feedback`
-  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`id_item`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `items_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `rent`
+--
+ALTER TABLE `rent`
+  ADD CONSTRAINT `rent_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `rent_ibfk_2` FOREIGN KEY (`id_item`) REFERENCES `items` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
